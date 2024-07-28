435969b51f294c508be5c90f4aae4ee1606e8710
zaadevofc <zaadevofc@gmail.com>
2024-07-28 11:48:57 +0700

    [UPDATE] move alignment aside

--- resources/views/dashboard/layouts.blade.php
+++ resources/views/dashboard/layouts.blade.php
@@ -1,8 +1,8 @@
 <div class="flex justify-between w-full gap-2 mt-4 sm:gap-5 max-lg:flex-col xl:gap-10 min-h-dvh">
-  @include('dashboard.asideLeft')
   <div class="lg:hidden">
     @include('dashboard.asideRight')
   </div>
+  @include('dashboard.asideLeft')
   @include('dashboard.content')
   <div class="max-lg:hidden">
     @include('dashboard.asideRight')
