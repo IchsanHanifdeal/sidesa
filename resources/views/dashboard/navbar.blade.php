903661476baf079b67a9f55f10124d01dc58db45
zaadevofc <zaadevofc@gmail.com>
2024-07-28 11:48:02 +0700

    [UPDATE] initial dropdown menu for profile and logout

--- resources/views/dashboard/navbar.blade.php
+++ resources/views/dashboard/navbar.blade.php
@@ -2,5 +2,23 @@
   <h1 class="text-xl font-bold">
     Hello, <span class="text-custom-blue">Kejaa</span>
   </h1>
-  <img class="flex-shrink-0 rounded-full size-10" src="https://avatars.githubusercontent.com/u/93970726?v=4" alt="">
+  <div class="dropdown dropdown-end">
+    <div tabindex="0" role="button">
+      <img class="flex-shrink-0 rounded-full size-10" src="https://avatars.githubusercontent.com/u/93970726?v=4" alt="">
+    </div>
+    <ul tabindex="0" class="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 drop-shadow-2xl border border-gray-300">
+      <li>
+        <a href="/profile" class="flex items-center gap-3">
+          <x-lucide-user-round class="flex-shrink-0 size-5" />
+          Profile
+        </a>
+      </li>
+      <li>
+        <a href="/profile" class="flex items-center gap-3">
+          <x-lucide-log-out class="flex-shrink-0 size-5" />
+          Logout
+        </a>
+      </li>
+    </ul>
+  </div>
 </nav>
