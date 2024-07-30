fc7b7c9c8bca2dd6e5e16876de02a0ab592c415a
ichsanhanifdeal <ichsanhanifdeal@gmail.com>
2024-07-30 16:22:09 +0700

    [ADD] Notification Announcement

--- app/Http/Controllers/PengumumanController.php
+++ app/Http/Controllers/PengumumanController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 
 class PengumumanController extends Controller
@@ -53,6 +54,8 @@ class PengumumanController extends Controller
      */
     public function update(Request $request, string $id)
     {
+        $user = Auth::user();
+        
         $request->validate([
             'image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
         ]);
@@ -75,6 +78,24 @@ class PengumumanController extends Controller
                 DB::table('pengumuman')->insert(['file' => $filePath, 'created_at' => now(), 'updated_at' => now()]);
             }
         }
+
+        if ($user->role === 'Admin') {
+            $users = DB::table('users')->where('id', '!=', $user->id)->get();
+
+            foreach ($users as $user) {
+                DB::table('notification')->insert([
+                    'name' => $user->name,
+                    'message' => 'Admin telah membuat pengumuman baru.',
+                    'to' => $user->id,
+                    'foto' => $user->image,
+                    'url' => '/pengumuman/' . ($pengumuman ? $pengumuman->id : DB::getPdo()->lastInsertId()),
+                    'read' => false,
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ]);
+            }
+        }
+
 
         return redirect()->back()->with('success', 'Pengumuman berhasil diperbarui!');
     }
